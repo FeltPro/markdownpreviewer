fe1a972817105fbb73357d1d9f1ea564a9a5aaad
FeltPro <86536306+FeltPro@users.noreply.github.com>
2021-10-14 19:26:30 +0100

    Markdown Previewer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import FormGroup from 'react-bootstrap/FormGroup'
+import FormLabel from 'react-bootstrap/FormLabel'
+import FormControl from 'react-bootstrap/FormControl'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MARKED = require("marked");
+
+class App extends Component {
+
+state ={
+  markdown:""
+}
+
+updateMarkdown = function(markdown){
+  this.setState({markdown});
+}
+
+  render() {
+    let { markdown } = this.state;
+    return (
+      <div className="App container">
+      <div>
+        <FormGroup controlId="formControlsTextarea">
+          <FormLabel>Markdown Input</FormLabel>
+          <FormControl componentClass="textarea" placeholder="Enter Text" value={ markdown } onChange= {(event)=>this.updateMarkdown(event.target.value)}></FormControl>
+        </FormGroup>
+      </div>
+      <div>
+        <h1>Markdown Output</h1>
+        <div dangerouslySetInnerHTML= {{__html: MARKED(markdown)}}></div>
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
